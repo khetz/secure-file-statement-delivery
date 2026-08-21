438503e0d3d8196a9e2eb1591aa929d5f3adf934
khetz <vukheta99@gmail.com>
2026-08-21 22:10:07 +0200

    Added application and infrastructure projectsa as well as the initial db context setup

---
+++ Infrastructure/Contexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AuditLog> AuditLogs { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<DownloadToken> DownloadTokens {  get; set; }
+    public DbSet<Statement> Statement { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Statement>()
+            .HasIndex(s => s.CustomerId);
+
+        modelBuilder.Entity<DownloadToken>()
+            .HasIndex(dt => dt.Token)
+            .IsUnique();
+
+        modelBuilder.Entity<DownloadToken>()
+            .HasIndex(dt => dt.ExpirationTime);
+
+    }
+}
