fb452562438979f4f8c9a664bc354c796b39ebaf
khetz <vukheta99@gmail.com>
2026-08-21 22:02:36 +0200

    Added and used dependency injection extension functions to bind config and specify the database type

---
+++ SecureFileStatementAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SecureFileStatementAPI.Configuration;
+
+namespace SecureFileStatementAPI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection BindConfig<T>(this IServiceCollection services, 
+        IConfiguration configuration, string sectionName) where T : class
+    {
+        services.Configure<T>(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+        {
+            var dbSettings = serviceProvider.GetRequiredService<IOptions<DataBaseConfig>>().Value;
+            options.UseSqlite(dbSettings.DefaultConnection);
+        });
+
+        return services;
+    }
+}
