72c84ed7c4868dcff3288f6494a89a604dca9ea9
khetz <vukheta99@gmail.com>
2026-08-21 22:01:04 +0200

    Added database config class to handle fetching database config section from app settings

---
+++ SecureFileStatementAPI/Configuration/DataBaseConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace SecureFileStatementAPI.Configuration;
+
+public class DataBaseConfig
+{
+    public const string DatabaseSectionName = "Database";
+    public string DefaultConnection { get; set; } = string.Empty;
+}
