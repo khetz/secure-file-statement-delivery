a42444288d91b5c6413fbb997ab41277c0fa9c3c
khetz <vukheta99@gmail.com>
2026-08-22 16:40:44 +0200

    Added file storage interface

---
+++ Application/Interfaces/Services/IFileStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces.Services;
+
+public interface IFileStorageService
+{
+    Task<string> StoreAsync(Stream fileContent, string storagePath);
+    Task<Stream> RetrieveAsync(string storagePath);
+    Task<bool> DeleteAsync(string storagePath);
+    Task<bool> ExistsAsync(string storagePath);
+}
