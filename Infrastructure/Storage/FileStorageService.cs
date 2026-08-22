98320ba7222e2bf33444a14dfb48bc9c0351a1c1
khetz <vukheta99@gmail.com>
2026-08-22 16:55:00 +0200

    Added file storage service initial implementation with base path fetching and file directory creation

---
+++ Infrastructure/Storage/FileStorageService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces.Services;
+using Infrastructure.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Storage;
+
+public class FileStorageService : IFileStorageService
+{
+    private readonly FileStorageConfig _fileStorageConfig;
+    private readonly string _basePath;
+
+    public FileStorageService(IOptions<FileStorageConfig> fileStorageConfig)
+    {
+        _fileStorageConfig = fileStorageConfig.Value;
+        _basePath = _fileStorageConfig.BasePath ?? throw new ArgumentNullException("Base path not configured");
+
+        Directory.CreateDirectory(_basePath);
+    }
+
+    public Task<bool> DeleteAsync(string storagePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsAsync(string storagePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> RetrieveAsync(string storagePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> StoreAsync(Stream fileContent, string storagePath)
+    {
+        throw new NotImplementedException();
+    }
+}
