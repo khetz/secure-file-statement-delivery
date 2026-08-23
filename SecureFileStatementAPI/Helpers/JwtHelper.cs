4a8e466c4518fce071ad612fc57537a5dbe9d7ef
khetz <vukheta99@gmail.com>
2026-08-23 15:43:35 +0200

    Added Jwt helper function class and function that generate the signing key

---
+++ SecureFileStatementAPI/Helpers/JwtHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SecureFileStatementAPI.Helpers;
+
+public static class JwtHelper
+{
+    public static SecurityKey GetSigningKey(string secret)
+    {
+        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+    }
+}
