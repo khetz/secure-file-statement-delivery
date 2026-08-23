c46bf00ac703bb703573e0e9d1e30b94e4bf1a5c
khetz <vukheta99@gmail.com>
2026-08-23 16:37:47 +0200

    Added jwt extension configuration and program cs call

--- SecureFileStatementAPI/Extensions/ServiceCollectionExtensions.cs
+++ SecureFileStatementAPI/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SecureFileStatementAPI.Configuration;
+using SecureFileStatementAPI.Helpers;
 
 namespace SecureFileStatementAPI.Extensions;
 
@@ -21,6 +23,30 @@ public static class ServiceCollectionExtensions
             var dbSettings = serviceProvider.GetRequiredService<IOptions<DataBaseConfig>>().Value;
             options.UseSqlite(dbSettings.DefaultConnection);
         });
+
+        return services;
+    }
+
+    public static IServiceCollection AddJWTAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = new JwtAuthenticationConfig();
+        configuration.GetSection(JwtAuthenticationConfig.JwtAuthenticationSectionName).Bind(jwtSettings);
+
+        services.AddAuthentication()
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = JwtHelper.GetSigningKey(jwtSettings.Secret)
+                };
+            });
 
         return services;
     }
--- SecureFileStatementAPI/Program.cs
+++ SecureFileStatementAPI/Program.cs
@@ -8,8 +8,12 @@ builder.Services
     .BindConfig<DataBaseConfig>(builder.Configuration, DataBaseConfig.DatabaseSectionName)
     .BindConfig<FileStorageConfig>(builder.Configuration, FileStorageConfig.StorageSectionName)
     .BindConfig<JwtAuthenticationConfig>(builder.Configuration, JwtAuthenticationConfig.JwtAuthenticationSectionName)
-    .AddDatabase();
+    .AddDatabase()
+    .AddJWTAuthentication(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
 
