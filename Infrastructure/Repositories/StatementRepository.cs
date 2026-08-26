4f8d4a8bdac8917d4010c4dffe5d105dfa6cab53
khetz <vukheta99@gmail.com>
2026-08-26 23:29:48 +0200

    Added statement repository with statement addition function

---
+++ Infrastructure/Repositories/StatementRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Repositories;
+
+public class StatementRepository : IStatementRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public StatementRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(Statement statement)
+    {
+        await _appDbContext.AddAsync(statement);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
