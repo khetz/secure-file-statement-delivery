13fae642848c89bd187577df919b886958781d29
khetz <vukheta99@gmail.com>
2026-08-26 22:41:20 +0200

    Added claims extensions file with customer id extraction from claims principal

---
+++ SecureFileStatementAPI/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace SecureFileStatementAPI.Extensions;
+
+public static class ClaimsExtensions
+{
+    public static Guid? GetCustomerId(this ClaimsPrincipal customer)
+    {
+        var customerId = customer.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        if (customerId == null) return null;
+
+        var parsedCustomerId = Guid.Parse(customerId);
+        return parsedCustomerId;
+    }
+}
