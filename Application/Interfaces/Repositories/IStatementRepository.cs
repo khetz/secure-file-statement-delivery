b00ab5df542fe90b201809f0801ce6d8a9c9c188
khetz <vukheta99@gmail.com>
2026-08-26 23:49:15 +0200

    Added get by customer id and get by statement id functions to statement repository

--- Application/Interfaces/Repositories/IStatementRepository.cs
+++ Application/Interfaces/Repositories/IStatementRepository.cs
@@ -5,4 +5,6 @@ namespace Application.Interfaces.Repositories;
 public interface IStatementRepository
 {
     Task<Guid> AddAsync(Statement statement);
+    Task<IReadOnlyCollection<Statement>> GetByCustomerIdAsync(Guid customerId);
+    Task<Statement?> GetByIsAsync(Guid statementId);
 }
--- Infrastructure/Repositories/StatementRepository.cs
+++ Infrastructure/Repositories/StatementRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
@@ -18,5 +19,16 @@ public class StatementRepository : IStatementRepository
         await _appDbContext.AddAsync(statement);
         await _appDbContext.SaveChangesAsync();
         return statement.Id;
+    }
+
+    public async Task<IReadOnlyCollection<Statement>> GetByCustomerIdAsync(Guid customerId)
+    {
+        var statements = _appDbContext.Statement.Where(s => s.CustomerId == customerId);
+        return await statements.ToListAsync();
+    }
+
+    public async Task<Statement?> GetByIsAsync(Guid statementId)
+    {
+        return await _appDbContext.Statement.FirstOrDefaultAsync(s => s.Id == statementId);
     }
 }
