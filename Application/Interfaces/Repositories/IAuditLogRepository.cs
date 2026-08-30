407bfebcf4cd9cea7e4d3590c5c37fb9d9504986
khetz <vukheta99@gmail.com>
2026-08-30 19:20:50 +0200

    Added audit log repo and addition function

---
+++ Application/Interfaces/Repositories/IAuditLogRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories;
+
+public interface IAuditLogRepository
+{
+    Task AddAsync(AuditLog auditLog);
+}
---
+++ Infrastructure/Repositories/AuditLogRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Repositories;
+
+public class AuditLogRepository : IAuditLogRepository
+{
+    private readonly AppDbContext _appDbContext;
+    public AuditLogRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(AuditLog auditLog)
+    {
+        await _appDbContext.AddAsync(auditLog);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
