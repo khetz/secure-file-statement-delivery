c9512ab29c1a9e8f7fb3534e94366b35b80e59a9
khetz <vukheta99@gmail.com>
2026-08-30 17:04:37 +0200

    Added token repository with addition function

---
+++ Infrastructure/Repositories/DownloadTokenRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Repositories;
+
+public class DownloadTokenRepository : IDownloadTokenRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public DownloadTokenRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task AddAsync(DownloadToken token)
+    {
+        await _appDbContext.AddAsync(token);
+        await _appDbContext.SaveChangesAsync();
+    }
+}
