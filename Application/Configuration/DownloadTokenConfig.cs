1a6fbdeec3afdb59dbf6e5a6b352d5ce1129ee6e
khetz <vukheta99@gmail.com>
2026-08-30 19:38:57 +0200

    Added base url to download token config

--- Application/Configuration/DownloadTokenConfig.cs
+++ Application/Configuration/DownloadTokenConfig.cs
@@ -5,4 +5,5 @@ public class DownloadTokenConfig
     public const string DowloadTokenSectionName = "DownloadToken";
     public string SigningKey { get; set; } = string.Empty;
     public int ExpiryMinutes { get; set; }
+    public string BaseUrl { get; set; }
 }
